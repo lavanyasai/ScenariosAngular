dfd57447b1972b26da9fe229245bf40e3d93a731
Lavanya Sai Pentela <lavanyasaipentela@gmail.com>
2019-08-07 09:31:38 +0530

    Scenario1

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { Scenario1Component1 } from './Scenario1/component1/component1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Scenario1Component1
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
